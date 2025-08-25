0c4479444127c925f3bb7e0379d20ae0f512da78
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-08-25 08:56:11 +0100

    Get the game setup working

--- game/screen.go
+++ game/screen.go
@@ -1,26 +1,23 @@
 package game
 
 import (
-	"fmt"
-	"image/color"
-
+	"github.com/gldraphael/ebit-ttt/core"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type screen struct {
-	// Define screen properties here
-	board []tile
+	board [3][3]tile
 }
 
 func (s *screen) Update() error {
-	// Implement screen update logic here
 	return nil
 }
 
 func (s *screen) Draw(sc *ebiten.Image) {
-	// Implement screen drawing logic here
-	for _, t := range s.board {
-		t.Draw(sc)
+	for i, row := range s.board {
+		for j, t := range row {
+			t.Draw(sc, core.Tile_Empty, i, j)
+		}
 	}
 }
 
@@ -29,18 +26,6 @@ func (s *screen) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func newScreen() *screen {
-	fmt.Println("Screen created")
 	g := &screen{}
-	for i := range 3 {
-		for j := range 3 {
-			img := ebiten.NewImage(50, 50)
-			img.Fill(color.RGBA{255, uint8(i * 10 / 255), uint8(j * 10 / 255), 255})
-			g.board = append(g.board, tile{
-				X:     float64(i * 50),
-				Y:     float64(j * 50),
-				Image: img,
-			})
-		}
-	}
 	return g
 }
--- game/tile.go
+++ game/tile.go
@@ -1,19 +1,31 @@
 package game
 
 import (
+	"image/color"
+
+	"github.com/gldraphael/ebit-ttt/core"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type tile struct {
-	// Define tile properties here
-	X     float64
-	Y     float64
-	Image *ebiten.Image
+type tile struct{}
+
+const TILE_SIZE = 100
+
+func (t *tile) Draw(screen *ebiten.Image, tile core.TileValue, x int, y int) {
+	image := getImageFor(tile)
+	options := &ebiten.DrawImageOptions{}
+	options.GeoM.Translate(float64(TILE_SIZE*x), float64(TILE_SIZE*y))
+	screen.DrawImage(image, options)
 }
 
-func (t *tile) Draw(sc *ebiten.Image) {
-	// Implement drawing logic here
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(t.X, t.Y)
-	sc.DrawImage(t.Image, op)
+func getImageFor(tile core.TileValue) *ebiten.Image {
+	img := ebiten.NewImage(TILE_SIZE, TILE_SIZE)
+	// TODO: draw tiles for X, Y, and EMPTY
+	img.Fill(color.RGBA{
+		R: 255,
+		G: 0,
+		B: 0,
+		A: 0,
+	})
+	return img
 }
